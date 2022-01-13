bf1584cf6f670346aca5dd48ce166757deb3f110
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-13 13:50:59 -0300

    Criaçao da tela e funçoes de soma

---
+++ src/gui/ViewController.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+	
+	@FXML
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		double number1 = Double.parseDouble(txtNumber1.getText());
+		double number2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + number2;
+		labelResult.setText(String.format("%.2f", sum));
+	}
+	
+}
