cccc18e8d24fd7c605ad5b3306af5475318966aa
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-13 13:59:14 -0300

    Aplicaçao da caixa de alerta caso digito invalido

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,9 +1,11 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 	
@@ -21,10 +23,15 @@ public class ViewController {
 	
 	@FXML
 	public void onBtSumAction() {
+		try {
 		double number1 = Double.parseDouble(txtNumber1.getText());
 		double number2 = Double.parseDouble(txtNumber2.getText());
 		double sum = number1 + number2;
 		labelResult.setText(String.format("%.2f", sum));
+		}
+		catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Digito invalido", "digite apenas numeros", AlertType.ERROR);
+		}
 	}
 	
 }
